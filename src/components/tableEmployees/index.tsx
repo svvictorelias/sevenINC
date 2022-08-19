c1b62b0c6c45496b873c19f74bf710bf97563cc4
Victor Elias <svvictorelias@gmail.com>
2022-08-19 09:40:18 -0300

    Feat: handleDelete

--- src/components/tableEmployees/index.tsx
+++ src/components/tableEmployees/index.tsx
@@ -2,43 +2,19 @@ import Paper from '@mui/material/Paper';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
-import IconButton from '@mui/material/IconButton';
-import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import TableContainer from '@mui/material/TableContainer';
-import MenuIcon from '@mui/icons-material/Menu';
 import DeleteIcon from '@mui/icons-material/Delete';
 import VisibilityIcon from '@mui/icons-material/Visibility';
 import ModeIcon from '@mui/icons-material/Mode';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
-import { useContext, useEffect, useState } from 'react';
+import { useContext} from 'react';
 import { MainContext } from '../../Provider';
 
 const TableEmployees = () => {
-  const { users } = useContext(MainContext);
-  const [rows, setRows] = useState();
-  useEffect(() => {
-    setRows(
-      users?.map(user => ({
-        id: user.id,
-        nome: user.nome,
-        email: user.email,
-        telefone: user.telefone,
-        salario: user.salario,
-        dataContratacao: user.dataContratacao
-      }))
-    );
-  }, [users]);
+  const { users, setUsers } = useContext(MainContext);
 
-  const [visibleButton, setVisibleButton] = useState<null | HTMLElement>(null);
-
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setVisibleButton(event.currentTarget);
-  };
-  const handleClose = () => {
-    setVisibleButton(null);
-  };
   const columns = [
     { id: 'nome', label: 'Nome', minWidth: 150 },
     { id: 'email', label: 'E-mail', minWidth: 150 },
@@ -51,6 +27,14 @@ const TableEmployees = () => {
       minWidth: 30
     }
   ];
+
+  //service
+  const handleDelete = (id)=>{
+    const newUsers = users.filter(user=>{
+      return user.id !==id
+    })
+    setUsers(newUsers)
+  }
 
   return (
     <Paper sx={{ width: '100%', overflow: 'hidden' }}>
@@ -66,7 +50,7 @@ const TableEmployees = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {rows?.map(item => (
+            {users?.map(item => (
               <TableRow key={item.id}>
                 <TableCell align="center">{item.nome}</TableCell>
                 <TableCell align="center">{item.email}</TableCell>
@@ -74,29 +58,20 @@ const TableEmployees = () => {
                 <TableCell align="center">{item.salario}</TableCell>
                 <TableCell align="center">{item.dataContratacao}</TableCell>
                 <TableCell align="center">
-                  <>
-                    <IconButton onClick={handleClick}>
-                      <MenuIcon />
-                    </IconButton>
-                    <Menu
-                      anchorEl={visibleButton}
-                      open={Boolean(visibleButton)}
-                      onClose={handleClose}
-                    >
-                      <MenuItem sx={{ gap: 2 }}>
-                        <ModeIcon />
-                        Editar dados
-                      </MenuItem>
-                      <MenuItem sx={{ gap: 2 }}>
-                        <VisibilityIcon />
-                        Mostrar detalhes
-                      </MenuItem>
-                      <MenuItem sx={{ gap: 2 }}>
-                        <DeleteIcon />
-                        Apagar funcionário
-                      </MenuItem>
-                    </Menu>
-                  </>
+                  <MenuItem sx={{ gap:1, display:'flex', justifyContent:'center' }}>
+                    <ModeIcon />
+                    Editar
+                  </MenuItem>
+                  <MenuItem sx={{ gap:1 , display:'flex', justifyContent:'center'}}>
+                    <VisibilityIcon />
+                    Exibir
+                  </MenuItem>
+                  <MenuItem
+                  onClick={()=>handleDelete(item.id)}
+                   sx={{ gap:1, display:'flex', justifyContent:'center' }}>
+                    <DeleteIcon />
+                    Apagar
+                  </MenuItem>
                 </TableCell>
               </TableRow>
             ))}
